469db398e9d005bc15c6eca129fd87616418f851
Negative <negggative@gmail.com>
2022-10-12 21:59:03 -0300

    Optimize imports

--- src/main/java/bot/soupbowl/commands/CommandPing.java
+++ src/main/java/bot/soupbowl/commands/CommandPing.java
@@ -1,12 +1,7 @@
 package bot.soupbowl.commands;
 
-import bot.soupbowl.Bot;
-import bot.soupbowl.config.EmbedsConfig;
-import com.sun.jdi.BooleanType;
 import games.negative.framework.discord.command.SlashCommand;
 import games.negative.framework.discord.command.SlashInfo;
-import games.negative.framework.discord.config.json.model.embed.JsonEmbedBuilder;
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 @SlashInfo(name = "ping", description = "Ping pong!")
