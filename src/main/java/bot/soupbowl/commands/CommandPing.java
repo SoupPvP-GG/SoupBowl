abed54320ac6e66a22369aa4033e0ddcdd4099b4
Negative <negggative@gmail.com>
2022-10-12 21:58:45 -0300

    Update CommandPing.java

--- src/main/java/bot/soupbowl/commands/CommandPing.java
+++ src/main/java/bot/soupbowl/commands/CommandPing.java
@@ -1,13 +1,23 @@
 package bot.soupbowl.commands;
 
+import bot.soupbowl.Bot;
+import bot.soupbowl.config.EmbedsConfig;
+import com.sun.jdi.BooleanType;
 import games.negative.framework.discord.command.SlashCommand;
 import games.negative.framework.discord.command.SlashInfo;
+import games.negative.framework.discord.config.json.model.embed.JsonEmbedBuilder;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 @SlashInfo(name = "ping", description = "Ping pong!")
 public class CommandPing extends SlashCommand {
+
+    public CommandPing() {
+        setCooldownFunction(event -> 5000L);
+    }
+
     @Override
     public void onCommand(SlashCommandInteractionEvent event) {
-        event.reply("Pong!").setEphemeral(true).queue();
+        event.reply("Pong!").queue();
     }
 }
